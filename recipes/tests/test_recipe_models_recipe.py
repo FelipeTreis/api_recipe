9e8163b6037ca6558234f9374b385c56d2502d75
Felipe Treis <felipetreis1@gmail.com>
2022-06-24 14:31:10 -0300

    Starting models tests

---
+++ recipes/tests/test_recipe_models_recipe.py
@@ -0,0 +1,14 @@
+from django.forms import ValidationError
+from recipes.tests.test_recipe_base import RecipeTestBase
+
+
+class RecipeModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.recipe = self.make_recipe()
+        return super().setUp()
+
+    def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
+        self.recipe.title = 'a' * 66
+
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
