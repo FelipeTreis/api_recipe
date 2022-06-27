8a9db443f97d79735e1eec8e616d1caf49e747f1
Felipe Treis <felipetreis1@gmail.com>
2022-06-27 13:55:36 -0300

    Tests refatoring

--- recipes/tests/test_recipe_models_recipe.py
+++ recipes/tests/test_recipe_models_recipe.py
@@ -14,7 +14,7 @@ class RecipeModelTest(RecipeTestBase):
             author=self.make_author(username='newuser'),
             title='Recipe title',
             description='Recipe description',
-            slug='recipe-slug',
+            slug='recipe-slug-for-no-default',
             preparation_time=10,
             preparation_time_unit='minutes',
             servings=5,
