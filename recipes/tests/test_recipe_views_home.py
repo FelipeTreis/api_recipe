9529f8b95ced2ad5278d568a3612e25d3702c6dc
Felipe Treis <felipetreis1@gmail.com>
2022-06-29 12:10:06 -0300

    Creating flash messages

--- recipes/tests/test_recipe_views_home.py
+++ recipes/tests/test_recipe_views_home.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.urls import resolve, reverse
 from recipes import views
 from recipes.tests.test_recipe_base import RecipeTestBase
@@ -39,3 +41,17 @@ class RecipeHomeViewsTest(RecipeTestBase):
 
         self.assertIn('<h1>No recipes found here 😭</h1>',
                       response.content.decode('utf-8'))
+
+    def test_recipe_home_is_paginated(self):
+        for i in range(6):
+            kwargs = {'slug': f'r{i}', 'author_data': {'username': f'u{i}'}}
+            self.make_recipe(**kwargs)
+
+        with patch('recipes.views.PER_PAGE', new=3):
+            response = self.client.get(reverse('recipes:home'))
+            recipes = response.context['recipes']
+            paginator = recipes.paginator
+
+        self.assertEqual(paginator.num_pages, 2)
+        self.assertEqual(len(paginator.get_page(1)), 3)
+        self.assertEqual(len(paginator.get_page(2)), 3)
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,6 @@
+import os
+
+from django.contrib import messages
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
@@ -5,14 +8,16 @@ from utils.pagination import make_pagination
 
 from recipes.models import Recipe
 
-PER_PAGES = 3
+PER_PAGE = int(os.environ.get('PER_PAGE', 3))
 
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
+    messages.success(request, 'Sucesso!')
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -24,7 +29,7 @@ def category(request, category_id):
     recipes = get_list_or_404(Recipe.objects.filter(
         category__id=category_id, is_published=True,).order_by('-id'))
 
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
@@ -55,7 +60,7 @@ def search(request):
         ), is_published=True
     ).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
