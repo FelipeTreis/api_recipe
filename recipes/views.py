c2e36bcacab93796c1040b6b206c54708b81f397
Felipe Treis <felipetreis1@gmail.com>
2022-06-29 14:06:27 -0300

    Removed messages in the views

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
 import os
 
-from django.contrib import messages
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
@@ -16,8 +15,6 @@ def home(request):
         is_published=True,).order_by('-id')
 
     page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
-
-    messages.success(request, 'Sucesso!')
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
