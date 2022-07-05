3d3693e6ef0968e56ce0e839647e9ef1fed07618
Felipe Treis <felipetreis1@gmail.com>
2022-07-05 11:30:20 -0300

    Test for email

--- authors/forms.py
+++ authors/forms.py
@@ -98,6 +98,14 @@ class RegisterForm(forms.ModelForm):
             'email',
         ]
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists
+
+        if exists:
+            raise ValidationError(
+                'User e-mail is already in use', code='invalid',)
+
     def clean(self):
         cleaned_data = super().clean()
 
