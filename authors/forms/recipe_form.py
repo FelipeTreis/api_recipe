0b4229df7f358f1b7febce440112962017a08977
Felipe Treis <felipetreis1@gmail.com>
2022-07-12 17:17:07 -0300

    Adjusting form and css

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,8 +1,14 @@
 from django import forms
 from recipes.models import Recipe
+from utils.django_forms import add_attr
 
 
 class AuthorRecipeForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        add_attr(self.fields.get('preparation_steps'), 'class', 'span-2')
+
     class Meta:
         model = Recipe
         fields = (
@@ -15,3 +21,15 @@ class AuthorRecipeForm(forms.ModelForm):
             'preparation_steps',
             'cover',
         )
+        widgets = {
+            'cover': forms.FileInput(attrs={'class': 'span-2'}),
+            'servings_unit': forms.Select(choices=(
+                ('Porções', 'Porções'),
+                ('Pedaços', 'Pedaços'),
+                ('Pessoas', 'Pessoas'),
+            )),
+            'preparation_time_unit': forms.Select(choices=(
+                ('Horas', 'Horas'),
+                ('Minutos', 'Minutos'),
+            ))
+        }
