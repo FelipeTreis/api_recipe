98c5a4bfe942dffe629b7b064d680d140465d4b3
Felipe Treis <felipetreis1@gmail.com>
2022-07-13 16:42:09 -0300

    Migrate FBV to CBV

--- recipes/tests/test_recipe_views_recipe.py
+++ recipes/tests/test_recipe_views_recipe.py
@@ -6,12 +6,12 @@ from recipes.tests.test_recipe_base import RecipeTestBase
 class RecipeRecipeViewsTest(RecipeTestBase):
 
     def test_recipe_recipe_view_function_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
-        self.assertIs(view.func, views.recipe)
+        view = resolve(reverse('recipes:recipe', kwargs={'pk': 1}))
+        self.assertIs(view.func.view_class, views.RecipeDetail)
 
     def test_recipe_recipe_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1000}))
+            reverse('recipes:recipe', kwargs={'pk': 1000}))
         self.assertEqual(response.status_code, 404)
 
     def test_recipe_recipe_template_loads_the_correct_recipes(self):
@@ -19,7 +19,7 @@ class RecipeRecipeViewsTest(RecipeTestBase):
         self.make_recipe(title=needed_title)
 
         response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1}))
+            reverse('recipes:recipe', kwargs={'pk': 1}))
         content = response.content.decode('utf-8')
 
         self.assertIn(needed_title, content)
@@ -28,6 +28,6 @@ class RecipeRecipeViewsTest(RecipeTestBase):
         recipe = self.make_recipe(is_published=False)
 
         response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': recipe.id}))
+            reverse('recipes:recipe', kwargs={'pk': recipe.id}))
 
         self.assertEqual(response.status_code, 404)
