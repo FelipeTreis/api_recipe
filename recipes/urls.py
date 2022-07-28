9b1f66424b06f2d6c2ce64a6d14db1edb15497dc
Felipe Treis <felipetreis1@gmail.com>
2022-07-28 15:11:44 -0300

    Created template, url and view to tag

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('', views.RecipeListViewHome.as_view(), name='home'),
     path('recipes/search/',
          views.RecipeListViewSearch.as_view(), name='search'),
+    path('recipes/tag/<slug:slug>/',
+         views.RecipeListViewTag.as_view(), name='tag'),
     path('recipes/category/<int:category_id>/',
          views.RecipeListViewCategory.as_view(), name='category'),
     path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name='recipe'),
@@ -15,5 +17,5 @@ urlpatterns = [
          name='recipes_api_v1'),
     path('recipes/api/v1/<int:pk>/', views.RecipeDetailApi.as_view(),
          name='recipes_api_v1_detail'),
-    path('recipes/theory', views.theory, name='theory'),
+    path('recipes/theory/', views.theory, name='theory'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -5,6 +5,7 @@ from django.forms.models import model_to_dict
 from django.http import Http404, JsonResponse
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
+from tag.models import Tag
 from utils.pagination import make_pagination
 
 from recipes.models import Recipe
@@ -33,6 +34,7 @@ class RecipeListViewBase(ListView):
         queryset = super().get_queryset(*args, **kwargs)
         queryset = queryset.filter(is_published=True,)
         queryset = queryset.select_related('author', 'category')
+        queryset = queryset.prefetch_related('tags')
 
         return queryset
 
@@ -161,3 +163,29 @@ class RecipeDetailApi(RecipeDetail):
         del recipe_dict['preparation_steps_is_html']
 
         return JsonResponse(recipe_dict, safe=False)
+
+
+class RecipeListViewTag(RecipeListViewBase):
+    template_name = 'recipes/pages/tag.html'
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(tags__slug=self.kwargs.get('slug', ''))
+
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        page_title = Tag.objects.filter(
+            slug=self.kwargs.get('slug', '')).first()
+
+        if not page_title:
+            page_title = 'Norecipes found'
+
+        page_title = f'{page_title} - Tag |'
+
+        context.update({
+            'page_title': page_title,
+        })
+
+        return context
