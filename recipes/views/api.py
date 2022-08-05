bee501fe0b1de694f558607cd19b50b0ea37be5c
Felipe Treis <felipetreis1@gmail.com>
2022-08-05 09:36:07 -0300

    Added neww methods http

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -16,25 +16,48 @@ def recipe_api_list(request):
             many=True,
             context={'request': request},
         )
+
         return Response(serializer.data)
 
     elif request.method == 'POST':
         serializer = RecipeSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+
         return Response(serializer.data,
                         status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(http_method_names=['get', 'patch', 'delete'])
 def recipe_api_detail(request, pk):
     recipe = get_object_or_404(Recipe.objects.get_published(), pk=pk)
-    serializer = RecipeSerializer(
-        instance=recipe,
-        many=False,
-        context={'request': request},
-    )
-    return Response(serializer.data)
+
+    if request.method == 'GET':
+        serializer = RecipeSerializer(
+            instance=recipe,
+            many=False,
+            context={'request': request},
+        )
+
+        return Response(serializer.data)
+
+    elif request.method == 'PATCH':
+        serializer = RecipeSerializer(
+            instance=recipe,
+            data=request.data,
+            many=False,
+            context={'request': request},
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data,)
+
+    elif request.method == 'DELETE':
+        recipe.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
@@ -45,4 +68,5 @@ def tag_api_detail(request, pk):
         many=False,
         context={'request': request},
     )
+
     return Response(serializer.data)
