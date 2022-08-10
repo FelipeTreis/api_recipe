3808afa22015d5e579abf92ec77dd5b86a996b8a
Felipe Treis <felipetreis1@gmail.com>
2022-08-10 14:45:25 -0300

    Overwriten create method

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from recipes.models import Recipe
 from recipes.permissions import IsOwner
 from recipes.serializers import RecipeSerializer, TagSerializer
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -56,6 +57,17 @@ class RecipeAPIv2ViewSet(ModelViewSet):
 
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
 
     def partial_update(self, request, *args, **kwargs):
         recipe = self.get_object()
